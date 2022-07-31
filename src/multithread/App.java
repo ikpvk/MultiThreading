1d7c2e36a381630591977bc5d66e35136a0d8ae1
Krishna Prasad <krishnaprasad.vk@gmail.com>
2022-07-31 23:51:32 +0530

    Multi Threading || Adding basic threads code

--- src/multithread/App.java
+++ src/multithread/App.java
@@ -1,7 +1,17 @@
 package multithread;
 
 public class App {
-    public static void main(String[] args) {
-        System.out.println("New program");
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Current thread " + Thread.currentThread().getName());
+            }
+        });
+
+        System.out.println("Current thread before start " + Thread.currentThread().getName());
+        t1.start();
+        System.out.println("Current thread after start " + Thread.currentThread().getName());
+        Thread.sleep(1000);
     }
 }
