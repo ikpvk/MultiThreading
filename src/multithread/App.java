c9efbfb3c803bec99987c0fb79db21ca84223d6e
Krishna Prasad <krishnaprasad.vk@gmail.com>
2022-07-31 23:59:23 +0530

    Multi Threading || Adding more code for multi threading

--- src/multithread/App.java
+++ src/multithread/App.java
@@ -5,13 +5,22 @@ public class App {
         Thread t1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                System.out.println("Current thread " + Thread.currentThread().getName());
+                System.out.println("Current thread " + Thread.currentThread().getName() +
+                        " priority " + Thread.currentThread().getPriority());
             }
         });
 
-        System.out.println("Current thread before start " + Thread.currentThread().getName());
+        //Setting custom name instead of default name
+        t1.setName("Thread test jarvis");
+
+        //Setting custom priority instead of default priority
+        t1.setPriority(Thread.MAX_PRIORITY);
+
+        System.out.println("Current thread before start " + Thread.currentThread().getName() +
+                " priority " + Thread.currentThread().getPriority());
         t1.start();
-        System.out.println("Current thread after start " + Thread.currentThread().getName());
+        System.out.println("Current thread after start " + Thread.currentThread().getName() +
+                " priority " + Thread.currentThread().getPriority());
         Thread.sleep(1000);
     }
 }
